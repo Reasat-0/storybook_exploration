46852c7d7e9720a09053a74f90c2cad39e2287df
reasat-0 <reasat.g8@gmail.com>
2023-10-08 12:49:17 +0600

    Init setup

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from ".";
+const meta: Meta<typeof Button> = {
+  title: "Components/Button",
+  component: Button,
+};
